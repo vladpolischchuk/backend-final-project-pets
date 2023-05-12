d3fe29e51d266948e9bed0a72923bd9c0f0af0f9
Matvey <benderisgreat32@gmail.com>
2023-05-13 01:56:59 +0300

    addNotice

--- models/notice.js
+++ models/notice.js
@@ -11,43 +11,43 @@ const noticesSchema = new Schema({
     type: String,
     required: true,
   },
-  // name: {
-  //   type: String,
-  //   minLength: 2,
-  //   maxLength: 16,
-  //   required: true,
-  // },
-  // birthday: {
-  //   type: String,
-  //   default: "12.05.2023",
-  //   required: true,
-  // },
-  // breed: {
-  //   type: String,
-  //   minLength: 2,
-  //   maxLength: 16,
-  //   required: true,
-  // },
-  // location: {
-  //   type: String,
-  //   required: true,
-  // },
-  // sex: {
-  //   type: String,
-  //   enum: ["male", "female"],
-  //   required: [true, "Choose the sex of animal"],
-  // },
-  // category: {
-  //   type: String,
-  //   enum: ["sell", "lost-found", "for-free"],
-  //   required: true,
-  // },
-  // price: {
-  //   type: String,
-  // },
-  // comments: {
-  //   type: String,
-  // },
+  name: {
+    type: String,
+    minLength: 2,
+    maxLength: 16,
+    required: true,
+  },
+  birthday: {
+    type: String,
+    default: "12.05.2023",
+    required: true,
+  },
+  breed: {
+    type: String,
+    minLength: 2,
+    maxLength: 16,
+    required: true,
+  },
+  location: {
+    type: String,
+    required: true,
+  },
+  sex: {
+    type: String,
+    enum: ["male", "female"],
+    required: [true, "Choose the sex of animal"],
+  },
+  category: {
+    type: String,
+    enum: ["sell", "lost-found", "for-free"],
+    required: true,
+  },
+  price: {
+    type: String,
+  },
+  comments: {
+    type: String,
+  },
   // image: {
   //   type: String,
   //   required: true,
@@ -71,27 +71,27 @@ noticesSchema.post("save", handleMongooseError);
 
 const addNoticeSchema = Joi.object(
   {
-    // category: Joi.string().valid("sell", "lost-found", "for-free").required(),
+    category: Joi.string().valid("sell", "lost-found", "for-free").required(),
     title: Joi.string().min(2).max(16).required(),
-    // birthday: Joi.string().pattern(birthdayRegexp),
-    // name: Joi.string().min(2).max(16).required(),
-    // breed: Joi.string().min(2).max(16).required(),
-    // sex: Joi.string().valid("male", "female").required(),
-    // location: Joi.string()
-    //   .pattern(locationRegexp)
-    //   .when("category", {
-    //     is: Joi.valid("sell", "lost-found", "for-free"),
-    //     then: Joi.required(),
-    //     otherwise: Joi.optional(),
-    //   }),
-    // price: Joi.number()
-    //   .min(0)
-    //   .when("category", {
-    //     is: "sell",
-    //     then: Joi.number().min(1).required(),
-    //     otherwise: Joi.optional(),
-    //   }),
-    // comments: Joi.string(),
+    birthday: Joi.string().pattern(birthdayRegexp),
+    name: Joi.string().min(2).max(16).required(),
+    breed: Joi.string().min(2).max(16).required(),
+    sex: Joi.string().valid("male", "female").required(),
+    location: Joi.string()
+      .pattern(locationRegexp)
+      .when("category", {
+        is: Joi.valid("sell", "lost-found", "for-free"),
+        then: Joi.required(),
+        otherwise: Joi.optional(),
+      }),
+    price: Joi.number()
+      .min(0)
+      .when("category", {
+        is: "sell",
+        then: Joi.number().min(1).required(),
+        otherwise: Joi.optional(),
+      }),
+    comments: Joi.string(),
   },
   { versionKey: false, timestamps: true }
 );
