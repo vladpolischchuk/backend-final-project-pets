1613b3f2b77bbe36a422e34b99da1c4af38e9807
Matvey <benderisgreat32@gmail.com>
2023-05-12 23:45:21 +0300

    add post/get notices

--- app.js
+++ app.js
@@ -4,6 +4,7 @@ const cors = require("cors");
 
 const authRouter = require("./routes/api/auth");
 const petsRouter = require("./routes/api/pets");
+const noticesRouter = require("./routes/api/notices");
 
 const app = express();
 
@@ -16,6 +17,7 @@ app.use(express.static("public"));
 
 app.use("/api/auth", authRouter);
 app.use("/api/pets", petsRouter);
+app.use("/api/notices", noticesRouter);
 
 app.use((req, res) => {
   res.status(404).json({ message: "Not found" });
--- models/notice.js
+++ models/notice.js
@@ -11,87 +11,87 @@ const noticesSchema = new Schema({
     type: String,
     required: true,
   },
-  name: {
-    type: String,
-    minLength: 2,
-    maxLength: 16,
-    required: true,
-  },
-  birthday: {
-    type: String,
-    default: "12.05.2023",
-    required: true,
-  },
-  breed: {
-    type: String,
-    minLength: 2,
-    maxLength: 16,
-    required: true,
-  },
-  location: {
-    type: String,
-    required: true,
-  },
-  sex: {
-    type: String,
-    enum: ["male", "female"],
-    required: [true, "Choose the sex of animal"],
-  },
-  category: {
-    type: String,
-    enum: ["sell", "lost-found", "for-free"],
-    required: true,
-  },
-  price: {
-    type: String,
-  },
-  comments: {
-    type: String,
-  },
-  image: {
-    type: String,
-    required: true,
-  },
-  owner: {
-    type: Schema.Types.ObjectId,
-    ref: "user",
-    required: true,
-  },
-  email: {
-    type: String,
-    required: true,
-  },
-  phone: {
-    type: String,
-    required: true,
-  },
+  // name: {
+  //   type: String,
+  //   minLength: 2,
+  //   maxLength: 16,
+  //   required: true,
+  // },
+  // birthday: {
+  //   type: String,
+  //   default: "12.05.2023",
+  //   required: true,
+  // },
+  // breed: {
+  //   type: String,
+  //   minLength: 2,
+  //   maxLength: 16,
+  //   required: true,
+  // },
+  // location: {
+  //   type: String,
+  //   required: true,
+  // },
+  // sex: {
+  //   type: String,
+  //   enum: ["male", "female"],
+  //   required: [true, "Choose the sex of animal"],
+  // },
+  // category: {
+  //   type: String,
+  //   enum: ["sell", "lost-found", "for-free"],
+  //   required: true,
+  // },
+  // price: {
+  //   type: String,
+  // },
+  // comments: {
+  //   type: String,
+  // },
+  // image: {
+  //   type: String,
+  //   required: true,
+  // },
+  // owner: {
+  //   type: Schema.Types.ObjectId,
+  //   ref: "user",
+  //   required: true,
+  // },
+  // email: {
+  //   type: String,
+  //   required: true,
+  // },
+  // phone: {
+  //   type: String,
+  //   required: true,
+  // },
 });
 
 noticesSchema.post("save", handleMongooseError);
 
 const addNoticeSchema = Joi.object(
   {
-    category: Joi.string().valid("sell", "lost-found", "for-free").required(),
+    // category: Joi.string().valid("sell", "lost-found", "for-free").required(),
     title: Joi.string().min(2).max(16).required(),
-    birthday: Joi.string().pattern(birthdayRegexp),
-    name: Joi.string().min(2).max(16).required(),
-    breed: Joi.string().min(2).max(16).required(),
-    sex: Joi.string().valid("male", "female").required(),
-    location: Joi.string()
-      .pattern(locationRegexp)
-      .when("category", {
-        is: Joi.valid("sell", "lost-found", "for-free"),
-        then: Joi.required(),
-        otherwise: Joi.optional(),
-      }),
-    price: Joi.number()
-      .min(0)
-      .when("category", {
-        is: "sell",
-        then: Joi.number().min(1).required(),
-        otherwise: Joi.optional(),
-      }),
-    comments: Joi.string(),
+    // birthday: Joi.string().pattern(birthdayRegexp),
+    // name: Joi.string().min(2).max(16).required(),
+    // breed: Joi.string().min(2).max(16).required(),
+    // sex: Joi.string().valid("male", "female").required(),
+    // location: Joi.string()
+    //   .pattern(locationRegexp)
+    //   .when("category", {
+    //     is: Joi.valid("sell", "lost-found", "for-free"),
+    //     then: Joi.required(),
+    //     otherwise: Joi.optional(),
+    //   }),
+    // price: Joi.number()
+    //   .min(0)
+    //   .when("category", {
+    //     is: "sell",
+    //     then: Joi.number().min(1).required(),
+    //     otherwise: Joi.optional(),
+    //   }),
+    // comments: Joi.string(),
   },
   { versionKey: false, timestamps: true }
 );
